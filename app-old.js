fffbcfa0abb69625d20e7b497d1a9f299f411e54
ivanhdzderas <ihernandez@rioroodfk.com>
2018-07-24 15:53:59 -0500

    primer commit

---
+++ app-old.js
@@ -0,0 +1,13 @@
+const http= require('http');
+http.createServer((req,res)=>{
+	res.writeHead(200,{'Content-Type':'application/json'});
+	let salida ={
+		nombre: 'Ivan',
+		edad:27,
+		url: req.url
+	}
+	res.write(JSON.stringify(salida));
+	res.end();
+})
+.listen(8080);
+console.log('escuchando puerto 8080');
